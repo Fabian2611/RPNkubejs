69d3ad8506a9ca39a5d3b72d0875342e5446e960
Fabian2611 <fabian.schwarz2010@gmail.com>
2025-07-06 11:36:58 +0200

    ADDed /doc steuer_firma, changed /doc steuer

--- server_scripts/src/documents.js
+++ server_scripts/src/documents.js
@@ -23,19 +23,17 @@ Vorname: Max\n\
 \n\
 Geburtstag:01.01.1800\n\
 \n\
-Geschlecht: m/w/d\n\
-\n\
-Hauptwohnsitz: Infinity\",\"----  Immobilien  ----\n\
+Hauptwohnsitz: [IC]\",\"----  Immobilien  ----\n\
 \n\
 -- Immobilie 1 --\n\
 Art: Wohnung / Haus\n\
-Adresse: Straße 1\n\
-Größe (m2): 100\n\
+Adresse: \n\
+Größe (m2): \n\
 \n\
 -- Immobilie 2 --\n\
 Art: Wohnung / Haus\n\
-Adresse: Straße 2\n\
-Größe (m2): 100 \",\"---- Einkommen ----\n\
+Adresse: \n\
+Größe (m2): \",\"---- Einkommen ----\n\
 \n\
 -- Beruf 1 --\n\
 Gehalt: 100$\n\
@@ -45,34 +43,49 @@ Tätigkeit: Müllmann\n\
 -- Beruf 2 --\n\
 Gehalt: 100$\n\
 Firma: Musterfirma 2\n\
-Tätigkeit: Müllfrau\",\"---- Umsatz ----\n\
-\n\
--- Firma 1 --\n\
-Name: Musterfirma\n\
-Sektor: Bauarbeiten\n\
-Umsatz: 300$\n\
-Umsatz Stände: 100$\n\
-Anzahl Mitarbeiter: 2 \n\
-\n\
--- Firma 2 --\n\
-Name: Musterfirma 2\n\
-Sektor: Finanzen\n\
-Umsatz: 1000$\n\
-Anzahl Mitarbeiter: 4\",\"-- Firmenausgaben --\n\
-\n\
--- Ausgabe 1 --\n\
-Höhe: 10$\n\
-Firma: Musterfirma\n\
-Grund: Auftrag / Büro\n\
-\n\
--- Ausgabe 2 --\n\
-Höhe: 20$\n\
-Firma: Musterfirma 2\n\
-Grund: Bürokosten\",\"---- Regeln ----\n\
+Tätigkeit: Müllfrau\",\"---- Regeln ----\n\
 \n\
 Sie sind verpflichtet ein mal im Monat eine Steuererklärung an das Büro des Finanzministeriums zu senden. Alle Angaben müssen wahrheitsgemäß sein. Steuerhinterziehung wird mit Haftstrafen geahndet. Das Finanzministerium\",\"kann gelegentlich auch Kontrollen durchführen, ob alle Steuern korrekt abgerechnet werden. Dafür können Firmen überwacht und durchleuchtet werden. Auch die Größe von Wohnungen und Häusern werden überprüft.\"]}")
 
     player.give(steuer)
+}
+
+function give_steuer_firma(player) {
+    let steuer_firma = Item.of('minecraft:writable_book', "{RepairCost:0,display:{Name:'{\"text\":\"Steuererklärung\"}'},pages:[\"--Steuererklärung--\n\
+\n\
+Name: Mustermann\n\
+\n\
+Vorname: Max\n\
+\n\
+Geburtstag:01.01.1800\",\"----  Umsatz  ----\n\
+\n\
+-- Firma 1 --\n\
+Name: Muster GmbH\n\
+Sektor: Produktion\n\
+Adresse: Testst. 1\n\
+Umsatz: 100.00$\n\
+\n\
+-- Firma 2 --\n\
+Name: \n\
+Sektor: \n\
+Adresse: \n\
+Umsatz: \",\"---- Ausgaben ----\n\
+\n\
+-- Muster GmbH --\n\
+Betrag: 100$\n\
+Grund: Baumaterial\n\
+\n\
+-- Muster GmbH --\n\
+Betrag: \n\
+Grund: \n\
+\n\
+-- Andere GmbH --\n\
+Betrag: \n\
+Grund: \",\"---- Regeln ----\n\
+\n\
+Dies ist ein Anhang zur Steuererklärung. Er muss von allen Firmeneigentümern mit der wöchentlichen Steuer abgegeben werden.\"]}")
+
+    player.give(steuer_firma)
 }
 
 function giveWaffen(player) {
@@ -227,6 +240,13 @@ ServerEvents.commandRegistry(event => {
                 .executes(context => {
                     const player = context.source.player;
                     giveFirma(player);
+                    return 1;
+                })
+            )
+            .then(Commands.literal("steuer_firma")
+                .executes(context => {
+                    const player = context.source.player;
+                    give_steuer_firma(player);
                     return 1;
                 })
             )
